dbf04c02d1711b3ba1dba18a8dbca8de575afbe0
valeria <vvqf2016@gmail.com>
2026-02-16 10:17:08 -0300

    test: remove SpringBootTest contextLoads to avoid issuer-uri dependency

--- src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/CulturalEventsApiApplicationTests.java
+++ src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/CulturalEventsApiApplicationTests.java
@@ -1,13 +1,10 @@
 package br.edu.ifrn.eventsapi.cultural_events_api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
 class CulturalEventsApiApplicationTests {
 
 	@Test
-	void contextLoads() {}
+	void contextLoads() {
+	}
 }
