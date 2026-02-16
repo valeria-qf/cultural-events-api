3fb99526c3334c0afe59c4a119a1aa1194d034aa
valeria <vvqf2016@gmail.com>
2026-02-16 07:03:18 -0300

    fix: add config

--- src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/TestSecurityConfig.java
+++ src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/TestSecurityConfig.java
@@ -1,9 +1,16 @@
+package br.edu.ifrn.eventsapi.cultural_events_api.support;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 @TestConfiguration
 @Profile("test")
 public class TestSecurityConfig {
 
     @Bean
-    @Primary
     SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
