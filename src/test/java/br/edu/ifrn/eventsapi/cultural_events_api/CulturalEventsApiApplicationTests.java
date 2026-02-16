41221243fcdf1565a4fb07076b9f617037ba6400
valeria <vvqf2016@gmail.com>
2026-02-16 05:39:47 -0300

    fix(test): activate test profile and configure OAuth2 issuer for tests

--- src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/CulturalEventsApiApplicationTests.java
+++ src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/CulturalEventsApiApplicationTests.java
@@ -2,12 +2,12 @@ package br.edu.ifrn.eventsapi.cultural_events_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CulturalEventsApiApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
