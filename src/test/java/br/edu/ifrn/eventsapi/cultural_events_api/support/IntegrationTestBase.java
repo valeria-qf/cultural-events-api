332503468f52a40ed82191c200facdb3e916377a
valeria <vvqf2016@gmail.com>
2026-02-16 06:17:01 -0300

    fix(tests): provide JwtDecoder for test profile to avoid Keycloak dependency

--- src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/IntegrationTestBase.java
+++ src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/IntegrationTestBase.java
@@ -1,7 +1,9 @@
 package br.edu.ifrn.eventsapi.cultural_events_api.support;
 
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,13 +11,9 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import org.springframework.context.annotation.Import;
-
 @Import(TestSecurityConfig.class)
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=" +
-                "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"
-})
+@SpringBootTest
+@AutoConfigureMockMvc
 @Testcontainers
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
