15e52dbebde41320d87fa8519f3933d8000186d9
valeria <vvqf2016@gmail.com>
2026-02-16 09:46:07 -0300

    test: run integration tests with H2 and embedded security config

--- src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/IntegrationTestBase.java
+++ src/test/java/br/edu/ifrn/eventsapi/cultural_events_api/support/IntegrationTestBase.java
@@ -1,7 +1,7 @@
 package br.edu.ifrn.eventsapi.cultural_events_api.support;
 
 import br.edu.ifrn.eventsapi.cultural_events_api.security.KeycloakRoleConverter;
-import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,48 +10,32 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Import(IntegrationTestBase.SecurityTestConfig.class)
 public abstract class IntegrationTestBase {
 
-    static final PostgreSQLContainer<?> POSTGRES =
-            new PostgreSQLContainer<>("postgres:16-alpine")
-                    .withDatabaseName("cultural_events_test")
-                    .withUsername("test")
-                    .withPassword("test");
-
-    @BeforeAll
-    static void start() {
-        POSTGRES.start();
-    }
-
-    @DynamicPropertySource
-    static void props(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-    }
-
     @TestConfiguration
     @EnableMethodSecurity
-    static class SecurityTestConfig {
+    public static class SecurityTestConfig {
 
         @Bean
-        SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+        public JwtDecoder jwtDecoder() {
+            return token -> Jwt.withTokenValue(token)
+                    .header("alg", "none")
+                    .claim("sub", "test")
+                    .build();
+        }
+
+        @Bean
+        public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
             JwtAuthenticationConverter jwtAuthConverter = new JwtAuthenticationConverter();
             jwtAuthConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
