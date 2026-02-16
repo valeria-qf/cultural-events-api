3fc0240320c6257d3ce201a859013429f9a19ed7
valeria <vvqf2016@gmail.com>
2026-02-16 00:54:19 -0300

    refactor(auth): remove legacy jwt auth implementation.

--- src/main/java/br/edu/ifrn/eventsapi/cultural_events_api/dto/request/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrn.eventsapi.cultural_events_api.dto.request;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank @Email String email,
-        @NotBlank String password
-) {}
--- src/main/java/br/edu/ifrn/eventsapi/cultural_events_api/dto/response/AuthResponse.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrn.eventsapi.cultural_events_api.dto.response;
-
-public record AuthResponse(
-        String token,
-        String tokenType,
-        Long userId,
-        String email,
-        String role
-) {}
