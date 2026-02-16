e3f711096a962a936a99814c14456e49fb2b06c8
valeria <vvqf2016@gmail.com>
2026-02-16 14:15:18 -0300

    fix: remove unused fields

--- src/main/java/br/edu/ifrn/eventsapi/cultural_events_api/dto/request/ReservationCreateRequest.java
+++ src/main/java/br/edu/ifrn/eventsapi/cultural_events_api/dto/request/ReservationCreateRequest.java
@@ -1,13 +1,9 @@
 package br.edu.ifrn.eventsapi.cultural_events_api.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ReservationCreateRequest(
         @NotNull Long sessionId,
-        @NotBlank String customerName,
-        @NotBlank @Email String customerEmail,
         @NotNull @Min(1) Integer quantity
 ) {}
